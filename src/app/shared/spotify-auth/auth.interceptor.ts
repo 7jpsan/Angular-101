518208a8048e386fd699a597d4db07e266f1f769
psoares-resilient <p.soares@resilientplc.com>
2018-03-12 09:45:49 +0000

    Make interceptor catch 401 responses

--- src/app/shared/spotify-auth/auth.interceptor.ts
+++ src/app/shared/spotify-auth/auth.interceptor.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { AuthorizeService } from './authorize.service';
 
 import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/do';
+import { Router } from '@angular/router';
+import { LoginService } from '.';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthorizeService) {}
+  constructor(private auth: AuthorizeService, private loginSvc: LoginService) {}
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authReq = req.clone({ setHeaders: this.auth.authHeader });
-    return next.handle(authReq);
+    
+    return next.handle(authReq).do((
+      event: HttpEvent<any>) => {}, 
+      (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          this.loginSvc.logout();
+        }
+      }
+    });
+    
+    //return next.handle(authReq);
   }
 }
